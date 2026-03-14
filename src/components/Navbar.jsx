c71021a42dd5e518e55471ec87bae65b01c32699
Shreyaa Katiyar <shreyaakatiyar@gmail.com>
2026-03-14 19:49:45 +0530

    add Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <nav className='sticky top-0 z-50 flex items-center justify-between px-8 py-4 surface glass border-b border-default'>
+                <span className='text-xl font-headline font-bold tracking-tight'>Dev<span className='color-primary'>Intel</span> </span>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
