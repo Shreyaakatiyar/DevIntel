aba7750dac284b9e5f6003eb3b89954fc0238527
Shreyaa Katiyar <shreyaakatiyar@gmail.com>
2026-03-15 17:28:31 +0530

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="py-4 text-center">
+        <p className="text-slate-500 text-sm">© 2026 DevIntel</p>
+    </footer> 
+  )
+}
+
+export default Footer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Features from '../components/Features'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Navbar/>
         <Hero/>
         <Features/>
+        <Footer/>
     </>
   )
 }
