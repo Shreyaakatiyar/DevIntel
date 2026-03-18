9d4e08365c20efb05f4eb24942a740c806c6f360
Shreyaa Katiyar <shreyaakatiyar@gmail.com>
2026-03-18 19:38:37 +0530

    fix recharts production build

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      'react-is': 'react-is',
+    }
+  },
+  optimizeDeps: {
+    include: [
+      'recharts',
+      'react-is',
+    ]
+  }
 })
