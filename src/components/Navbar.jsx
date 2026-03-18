be1abb957d4a123ba00009134d863bb0186ef4fb
Shreyaa Katiyar <shreyaakatiyar@gmail.com>
2026-03-18 19:34:03 +0530

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Logo from "../assets/Logo.svg"
 
 const Navbar = () => {
   return (
@@ -6,7 +7,7 @@ const Navbar = () => {
         <nav className='top-0 z-50 flex items-center justify-between px-8 py-4 surface glass border-b border-[#282828]'>
             <div className='flex items-center gap-2 justify-center'>
                 <div className='w-11 h-11 rounded-lg flex items-center justify-center'>
-                    <span><img src="./src/assets/Logo.svg" alt="Logo"/></span>
+                    <span><img src={Logo} alt="Logo"/></span>
                 </div>
                 <span className='text-2xl font-headline font-bold tracking-tight text-primary'>DevIntel</span>
             </div>
