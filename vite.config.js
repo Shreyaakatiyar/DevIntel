709b2c621bf5d13851c4421bbfc44e7232e1c7ff
Shreyaa Katiyar <shreyaakatiyar@gmail.com>
2026-03-18 19:57:20 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -6,18 +6,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      external: ['recharts'],
-      output: {
-        globals: {
-          recharts: 'Recharts'
-        }
-      }
+  resolve: {
+    alias: {
+      'react-is': 'react-is',
     }
   },
   optimizeDeps: {
-    exclude: ['recharts']
+    include: [
+      'recharts',
+      'react-is',
+    ]
   }
 })
