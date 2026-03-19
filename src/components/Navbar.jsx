3bc96f302d654a989062a7a675199b3b9233235c
Shreyaa Katiyar <shreyaakatiyar@gmail.com>
2026-03-19 17:21:59 +0530

    add logo responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,16 @@ import Logo from "../assets/Logo.svg"
 
 const Navbar = () => {
   return (
-    <>
-        <nav className='w-full fixed top-0 z-50 flex items-center justify-between px-8 py-4 surface glassbg-[#1d1c1c]'>
-            <div className='flex items-center gap-2 justify-center'>
-                <div className='w-11 h-11 rounded-lg flex items-center justify-center'>
-                    <span><img src={Logo} alt="Logo"/></span>
-                </div>
-                <span className='text-2xl font-headline font-bold tracking-tight text-primary'>DevIntel</span>
-            </div>
-        </nav>
-    </>
+    <nav className='w-full fixed top-0 z-50 flex items-center justify-between px-4 md:px-8 py-3 md:py-4 surface glass bg-[#1d1c1c]'>
+      <div className='flex items-center gap-2'>
+        <div className='w-7 h-7 md:w-11 md:h-11 rounded-lg flex items-center justify-center'>
+          <img src={Logo} alt="Logo" className="w-full h-full" />
+        </div>
+        <span className='text-lg md:text-2xl font-headline font-bold tracking-tight text-primary'>
+          DevIntel
+        </span>
+      </div>
+    </nav>
   )
 }
 
